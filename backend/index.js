142a1288739a233b02f2e47fbc97fa4931a6cc56
sushruta19 <sensoubhik2001@gmail.com>
2024-04-18 17:02:09 +0530

    Added the rest of HTTP Routes and Tested them

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,71 @@ app.post("/books", async (req, res) => {
   }
 });
 
+//Route to get all books from database
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    res.status(200).json({
+      count : books.length,
+      data : books
+    });
+  } catch(error) {
+    console.error("Error in GET /books route", error);
+    res.status(500).send({ message : error.message });
+  }
+});
 
+//Route to get one book from database by id
+app.get("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if(!book) {
+      return res.status(404).json({message : "Not Found"});
+    }
+    res.status(200).json(book);
+  } catch(error) {
+    console.error("Error in GET /books/:id route", error);
+    res.status(500).send({ message : error.message });
+  }
+});
+
+//Route to update a book by ID
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({
+        message : 'Send all required fields: title, author, publishYear'
+      });
+    }
+    const result = await Book.findByIdAndUpdate(req.params.id, req.body);
+    if(!result) {
+      return res.status(404).json({message : "Book Not Found"});
+    }    
+    res.status(200).json({message : "Successfully Updated Book"});
+
+  } catch(error) {
+    console.error("Error in PUT /books/:id route", error);
+    res.status(500).send({ message : error.message });
+  }
+})
+
+//Route to delete a book by ID
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const result = await Book.findByIdAndDelete(req.params.id);
+    if(!result) {
+      return res.status(404).json({message : "Book Not Found"});
+    }    
+    res.status(200).json({message : "Successfully Deleted Book"});
+  } catch(error) {
+    console.error("Error in DELETE /books/:id route", error);
+    res.status(500).send({ message : error.message });
+  }
+})
 
 process.on("SIGINT", async () => {
   try {
