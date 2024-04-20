f99eb027ca0c3ca67c1726243e57d59b13fa5b7a
sushruta19 <sensoubhik2001@gmail.com>
2024-04-20 21:13:13 +0530

    Create EditBook and CreateBook components Correct linkto of CreateBook in home component Position spinner at centre of page

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Spinner() {
   return (
-    <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'></div>
+    <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center h-screen'>
+      <div className='absolute animate-ping w-16 h-16 rounded-full bg-sky-600'></div>
+    </div>
   )
 }
 export default Spinner;
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,8 +1,91 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+
 
 function EditBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+  
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:7000/books/${id}`)
+      .then(response => {
+        setTitle(response.data.title);
+        setAuthor(response.data.author);
+        setPublishYear(response.data.publishYear);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleEditBook = () => {
+    setLoading(true);
+    const data = {
+      title : title,
+      author : author,
+      publishYear : publishYear
+    };
+    axios
+      .put(`http://localhost:7000/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch(error => {
+        setLoading(false);
+        alert("An error happened! Please check console.");
+        console.error(error);
+      });
+  }
   return (
-    <div>EditBook</div>
-  )
+    <div className='p-4'>
+      <BackButton/>
+      <h1 className="text-3xl my-4">Edit Book</h1>
+      {loading ? <Spinner/>:''}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label htmlFor="" className='text-xl mr-4 text-gray-500'>Title</label>
+          <input
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className="my-4">
+          <label htmlFor="" className='text-xl mr-4 text-gray-500'>Author</label>
+          <input
+            type='text'
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className="my-4">
+          <label htmlFor="" className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input
+            type='text'
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
+          Edit
+        </button>
+      </div>
+    </div>
+  );
 }
 export default EditBook;
