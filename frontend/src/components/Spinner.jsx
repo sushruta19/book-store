829cb05ccd1e363a73b6f0f7a2f8e13148cbde4a
sushruta19 <sensoubhik2001@gmail.com>
2024-04-21 11:20:58 +0530

    Create DeleteBook page Removed one useless class from Spinner component

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Spinner() {
   return (
-    <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center h-screen'>
+    <div className='fixed top-0 left-0 w-full flex justify-center items-center h-screen'>
       <div className='absolute animate-ping w-16 h-16 rounded-full bg-sky-600'></div>
     </div>
   )
